3f625fc711a6022cbbd40e2c62717882e7989a7f
Raed Abdul Majeed <100485450+raedmajeed@users.noreply.github.com>
2024-03-22 06:15:24 +0530

    fix: added log details and other info for easy debug

--- config/config.go
+++ config/config.go
@@ -1,9 +1,6 @@
 package config
 
 import (
-	"io"
-	"log"
-	"net/http"
 	"os"
 
 	"github.com/go-redis/redis/v8"
@@ -22,7 +19,6 @@ type ConfigParams struct {
 	BUSINESSSURGE    string `mapstructure:"BUSINESSSURGE"`
 	ADMINBOOKINGPORT string `mapstructure:"ADMINBOOKINGPORT"`
 	KAFKABROKER      string `mapstructure:"KAFKABROKER"`
-	DOCKERVERSION    string `mapstructure:"DOCKERVERSION"`
 }
 
 func Configuration() (*ConfigParams, error, *redis.Client) {
@@ -43,11 +39,7 @@ func Configuration() (*ConfigParams, error, *redis.Client) {
 	cfg.BUSINESSSURGE = os.Getenv("BUSINESSSURGE")
 	cfg.ADMINBOOKINGPORT = os.Getenv("ADMINBOOKINGPORT")
 	cfg.KAFKABROKER = os.Getenv("KAFKABROKER")
-	cfg.DOCKERVERSION = os.Getenv("DOCKERVERSION")
 
-	if cfg.DOCKERVERSION == "" {
-		log.Fatal("error configuring env values: ", cfg)
-	}
 	redis2 := connectToRedis(&cfg)
 	return &cfg, nil, redis2
 }
@@ -59,18 +51,4 @@ func connectToRedis(cfg *ConfigParams) *redis.Client {
 		DB:       2,
 	})
 	return client
-}
-
-func GetDockerTag() string {
-	apiUrl := "http://localhost/info"
-	resp, err := http.Get(apiUrl)
-	if err != nil {
-		log.Fatal("Failed to fetch Docker info:", err)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Failed to read response body:", err)
-	}
-	return string(body)
 }
